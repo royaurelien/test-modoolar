86fc46a6fb054867e8cedd74afb129f5ef736129
Gabriel Santos <gsantos@yziact.fr>
2017-11-24 16:07:14 +0100

    dom_products

---
+++ dom_products/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+{
+    'name': "Domitec Produits",
+
+    'summary': """ Modification des Produits """,
+
+    'description': """ """,
+
+    'author': "G. Santos",
+    'website': "http://www.yziact.fr",
+
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'product',
+    ],
+
+    # always loaded
+    'data': [
+    ],
+}
+
