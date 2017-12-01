bdd907a5d4e6eea18d924c1e96be519ff23780a6
Chabagutor <bchaudat@yziact.fr>
2017-12-01 16:16:16 +0100

    [IMP]fiche client : Ajout du code qui split l onglet contact en deux et qui séparre les contact des filiales

--- dom_partner/models/res_partner.py
+++ dom_partner/models/res_partner.py
@@ -4,16 +4,28 @@ from odoo import api, fields, models, tools
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
+    def get_child_ids(self):
+        vals = []
+
+        for child in self.child_ids :
+            if child.company_type == 'company':
+                vals.append(child)
+
+        return vals
+
     #### RELATIONNEL ####
     famille = fields.Many2one(comodel_name='dom.famille', string='Famille client')
     contact1 = fields.Many2one(comodel_name='res.partner', string='Contact 1')
     contact2 = fields.Many2one(comodel_name='res.partner', string='Contact 2')
     contact3 = fields.Many2one(comodel_name='res.partner', string='Contact 3')
     transporteur = fields.Many2one(comodel_name='dom.transporteur', string='Transporteur')
-
+    child_ids_2 = fields.One2many('res.partner', 'parent_id', string='Filiale',
+                                  domain=[('active', '=', True), ('company_type', '=', 'company')],
+                                  default=get_child_ids)
     #### TEXT ####
     code_api = fields.Char(string='Code API')
     horaires_livraison = fields.Text(string="Jours et horaires de livraisons")
+    fax = fields.Char(string="fax")
 
     #### NUMERIQUE ####
     ca_12 = fields.Integer(string='CA 12 mois')
@@ -36,6 +48,7 @@ class ResPartner(models.Model):
         ('6','6 mois'),
         ('12','12 mois')
     ], string=u"Fréquence de contact")
+    company_type = fields.Selection(store=True)
 
     #### ONCHANGE ####
     @api.onchange('famille')
