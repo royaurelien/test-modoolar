f969965d50e7493bd215b6fdb687a0929718a942
Baptiste <bchaudat@yziact.fr>
2018-02-07 11:24:28 +0100

    [NEW]script import grand livre

---
+++ import_scripts/import_grand_livre.py
@@ -0,0 +1,144 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import xmlrpclib
+import csv
+import sys
+from datetime import datetime
+import pprint
+
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+
+# Connexion Odoo
+username = "admin"
+pwd = "X200yziact"
+dbname = "DOM_07_02"
+
+sock_common = xmlrpclib.ServerProxy("http://192.168.100.139:8069/xmlrpc/common")
+uid = sock_common.login(dbname, username, pwd)
+sock = xmlrpclib.ServerProxy("http://192.168.100.139:8069/xmlrpc/object")
+
+def format_date(date_str):
+    if not date_str :
+        raise UserWarning('PAS de DATE')
+
+    day, month, year = date_str.split('/')
+
+    date = year+'-'+month+'-'+day
+
+    return date
+
+fich_ = open('Grand livre v2.csv', 'rb')
+
+csvreader = csv.reader(fich_, delimiter=';')
+
+tot = 0
+i = 1
+
+dict_journaux = {
+    'VE' : 1,
+    'AC' : 2,
+    'CEE' : 4,
+    'CE' : 7,
+    '90' : 9,
+    'BS' : 10,
+    'SAL' : 13,
+    'FG' : 12,
+}
+
+list_move = []
+
+for row in csvreader:
+    # print(i)
+    if i <=1:
+        i+=1
+        continue
+
+    tuple_ecriture = ()
+    dict_ecriture = {}
+    move_dict = {}
+    debit = 0.0
+    credit = 0.0
+
+    date_str = row[0]
+    date = format_date(date_str)
+    name = row[2]
+    # print(name)
+    code_jour = row[3]
+    journal_id = dict_journaux[code_jour]
+    line_ids = []
+
+    compte = row[1]
+    libel = row[6]
+    debit_str = (row[7].replace(',', '.')).replace(' ', '')
+    credit_str = (row[8].replace(',', '.')).replace(' ', '')
+    if debit_str:
+        debit = float(debit_str)
+    if credit_str:
+        credit = float(credit_str)
+
+    compte_id = sock.execute(dbname, uid, pwd, 'account.account', 'search', ([('code', '=', compte)]))
+    if not compte_id:
+        print(compte)
+        continue
+
+    dict_ecriture = {
+        'date': date,
+        'account_id': compte_id[0],
+        'name': libel,
+    }
+
+    if str(debit) != '0.0':
+        dict_ecriture['debit'] = debit
+
+    if str(credit) != '0.0':
+        dict_ecriture['credit'] = credit
+
+
+    tuple_ecriture = (0, 0, dict_ecriture)
+
+    j=0
+    test = False
+    for move in list_move:
+        if move['name'] != name or move['journal_id'] != journal_id:
+            j+=1
+            # print(test)
+        else:
+            test = True
+            # print(test)
+            break
+
+    if not test:
+        line_ids.append(tuple_ecriture)
+        move_dict ={
+            'date':date,
+            'name':name,
+            'journal_id':journal_id,
+            'line_ids':line_ids,
+        }
+        list_move.append(move_dict)
+    else:
+        # print(j)
+        list_move[j]['line_ids'].append(tuple_ecriture)
+
+    i+=1
+
+
+# pprint.pprint(list_move)
+fail = []
+for move in list_move:
+    # pprint.pprint(move)
+    print(move['name'],move['journal_id'])
+    if (move['name'] == '' and move['journal_id'] == 7) or (move['name'] == '' and move['journal_id'] == 9) or len(move['line_ids'])<2 or len(move['name'])<5 or move['name']=='17092124':
+        fail.append((move['name'],move['journal_id']))
+
+        continue
+
+    sock.execute(dbname, uid, pwd, 'account.move', 'create', move)
+    tot+=1
+    print(tot)
+
+print(tot)
