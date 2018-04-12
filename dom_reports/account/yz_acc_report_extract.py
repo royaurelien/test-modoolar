4b8a8c8e36faeb5f6b02842bedb29279e0d3d9b7
Claire CAPARROS <ccaparros@yziact.local>
2018-04-12 15:50:20 +0200

    [CHG] Réécriture des commentaires
    Suppression des caractères accentués ou pouvant poser problème à la
    compilation.

--- dom_reports/account/yz_acc_report_extract.py
+++ dom_reports/account/yz_acc_report_extract.py
@@ -22,62 +22,63 @@ class YzAccReportExtract(models.TransientModel):
     @api.multi
     def action_export(self, date_deb, date_f, account):
         """
-        :param date_deb: date de début pour la séléction
-        :param date_f: date de fin pour la séléction
-        :param account: compte à utiliser pour la séléction
-        :return liste des lignes à afficher sur le report
+        :param date_deb: date de debut pour la selection
+        :param date_f: date de fin pour la selection
+        :param account: compte a utiliser pour la selection
+        :return liste des lignes a afficher sur le report
         """
-        account_move_env = self.env['account.move.line']                                                                                # Récupération du modèle
-        data_move_line = account_move_env.search([('date', '>=', date_deb), ('date', '<=', date_f), ('account_id', '=', account.id)])   # Séléction des lignes à afficher sur le pdf
+
+        account_move_env = self.env['account.move.line']                                                                                # Recuperation du modele
+        data_move_line = account_move_env.search([('date', '>=', date_deb), ('date', '<=', date_f), ('account_id', '=', account.id)])   # Selection des lignes a afficher sur le pdf
 
         return data_move_line
 
     @api.multi
     def action_export_extract(self):
         """
-        :return une action act_url pour télécharger le pdf
+        :return une action act_url pour telecharger le pdf
         """
 
         if self.date_debut > self.date_fin:
             raise UserError(_('La date de début doit être inférieure ou égale à la date de fin.'))
         else:
-            data_move_lines = self.action_export(self.date_debut, self.date_fin, self.account_id)       # Séléction des lignes à afficher
+            data_move_lines = self.action_export(self.date_debut, self.date_fin, self.account_id)       # Selection des lignes a afficher
 
 
-            # Récupération des ids de chaque ligne à afficher
+            # Recuperation des ids de chaque ligne a afficher
             docids = []
 
             for move in data_move_lines:
                 docids.append(move.id)
 
 
-            # Récupération du template à utiliser
+            # Recuperation du template a utiliser
             report_obj = self.env['ir.actions.report']
             report = report_obj._get_report_from_name('dom_reports.dom_report_account_line')
 
 
-            # Dictionnaire indiquant les lignes à afficher
+            # Dictionnaire indiquant les lignes a afficher
             docargs = {
                 'doc_ids': docids,
                 'doc_model': report.model,
                 'docs': data_move_lines,
             }
 
-            html = report.render_qweb_html(docids, docargs)                                             # Génération du document sous forme html
+            html = report.render_qweb_html(docids, docargs)                                             # Generation du document sous forme html
 
-            bodies, res_ids, header, footer, specific_paperformat_args = report._prepare_html(html[0])  # Division du html en différentes parties
+            bodies, res_ids, header, footer, specific_paperformat_args = report._prepare_html(html[0])  # Division du html en differentes parties
 
-            mu = report._run_wkhtmltopdf(bodies, header, footer, specific_paperformat_args)             # Assemblage des parties et génération du pdf
+            mu = report._run_wkhtmltopdf(bodies, header, footer, specific_paperformat_args)             # Assemblage des parties et generation du pdf
 
 
-            # Encodage du pdf et détermination du nom de fichier
+            # Encodage du pdf et determination du nom de fichier
             self.write({
                 'value': base64.encodestring(mu),
                 'filename': 'Extrait de compte',
              })
 
 
-            # Ouverture de la fenêtre de téléchargement du pdf
+            # Ouverture de la fenetre de telechargement du pdf
             return {
                      "type": "ir.actions.act_url",
                      "url": "web/content/?model=yz.acc.report.extract&id=" + str(
