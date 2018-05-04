ff8c70953bd1d312582fb75546e9f7545ee3a0ce
ROOT DOM <support-si@yziact.fr>
2018-05-04 11:43:29 +0200

    [HFIX] Hotfix, articles inaccessibles

--- filters_yzi/models/filters.py
+++ filters_yzi/models/filters.py
@@ -17,16 +17,6 @@ class SaleReport(models.Model):
 
     partner_id = fields.Many2one('res.partner', delegate=True)
 
-    # Test
-    # requested_date = fields.Datetime(related='sale.order')
-    requested_date_ = fields.Many2one("sale.order",  delegate=True, read_only=True)
-    #amount_total = fields.Float(related='sale.order', read_only=True)
-    #picking_ids = fields.Many2one(related='sale.order', inverse='get_true')
-    #invoice_status = fields.Char(related="invoice_ids.state", string="statut commande", read_only="True")
-
-    def get_true(self):
-        return True
-
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
