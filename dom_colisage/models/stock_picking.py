68729c306635f11ecebb429f6d0b42aaa4c1f1d3
Claire CAPARROS <ccaparros@yziact.local>
2018-05-29 14:17:16 +0200

    [CHG] Calcul nombre de cartons, export Schenker

--- dom_colisage/models/stock_picking.py
+++ dom_colisage/models/stock_picking.py
@@ -18,9 +18,14 @@ class StockPicking(models.Model):
         for rec in self:
 
             items = []
+
             for line in rec.move_lines :
                 nb_par_colis = line.product_id.nb_par_colis
                 nb_delivering = line.quantity_done
+
+                if line.product_id.is_comment == True or 'palette' in line.product_id.name or 'Palette' in line.product_id.name or 'PALETTE' in line.product_id.name:
+                    nb_delivering = 0
+
                 items.append(Item(nb_par_colis, nb_delivering))
 
             rec.nb_cartons = CartonsComputer(items).get_num_cartons()
@@ -53,6 +58,11 @@ class StockMove(models.Model):
         for rec in self:
             nb_par_colis = rec.product_id.nb_par_colis
             nb_delivering = rec.quantity_done
+
+            if rec.product_id.is_comment == True or 'palette' in rec.product_id.name or 'Palette' in rec.product_id.name or 'PALETTE' in rec.product_id.name:
+                nb_delivering = 0
+
             items.append(Item(nb_par_colis, nb_delivering))
 
             rec.nb_cartons = CartonsComputer(items).get_num_cartons()
+
