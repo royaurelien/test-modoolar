c128a5775948f3bd5469da63c08684236f36e055
Baptiste <bchaudat@yziact.fr>
2018-06-05 17:25:52 +0200

    [NEW] Debut du module dom_sale custom views/action pour domitec

---
+++ dom_sale/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+{
+    'name': "Modification Sale pour Domitec",
+
+    'summary': """ Modification Sale pour Domitec """,
+
+    'description': """ Custom de view et filtre et autre""",
+
+    'author': "Yziact, Baptiste",
+
+    'version': '0.1',
+
+    'category': 'Sales',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'sale',
+        'sale_order_dates',
+    ],
+
+    # always loaded
+    'data': [
+        'views/devis.xml'
+    ],
+
+    # 'installable': True,
+}
