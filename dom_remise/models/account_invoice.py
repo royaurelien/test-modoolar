f42ca2cbd083330c590cc40a1f56f4f1499abd2b
Baptiste <bchaudat@yziact.fr>
2018-10-05 09:31:41 +0200

    [IMP] DOM-100: Chiffres raport compta

--- dom_remise/models/account_invoice.py
+++ dom_remise/models/account_invoice.py
@@ -100,19 +100,46 @@ class InvoiceTarifLine(models.Model):
 
             line.update(vals)
 
-"""
 
-class InvoiceTarifTax(models.Model):
-    _inherit = 'account.invoice.tax'
+class AccountInvoiceReport(models.Model):
+    _inherit = 'account.invoice.report'
 
-    @api.depends('amount', 'amount_rounding')
-    def _compute_amount_total(self):
-        super(InvoiceTarifTax, self)._compute_amount_total()
+    price_total_net = fields.Float(string='Total Without Tax Net', readonly=True)
 
-        for tax_line in self:
-            if tax_line.invoice_id.remise:
-                remise = tax_line.invoice_id.remise.amount
+    def _select(self):
+        select_str = """
+            SELECT sub.id, sub.date, sub.product_id, sub.partner_id, sub.country_id, sub.account_analytic_id,
+                sub.payment_term_id, sub.uom_name, sub.currency_id, sub.journal_id,
+                sub.fiscal_position_id, sub.user_id, sub.company_id, sub.nbr, sub.type, sub.state,
+                sub.categ_id, sub.date_due, sub.account_id, sub.account_line_id, sub.partner_bank_id,
+                sub.product_qty, sub.price_total as price_total,
+                sub.price_total_net as price_total_net,
+                sub.price_average as price_average,
+                COALESCE(cr.rate, 1) as currency_rate, sub.residual as residual, sub.commercial_partner_id as commercial_partner_id
+        """
+        return select_str
 
-                tax_line.amount_total = tax_line.amount_total - tax_line.amount_total * (remise / 100)
-"""
-
+    def _sub_select(self):
+        select_str = """
+                SELECT ail.id AS id,
+                    ai.date_invoice AS date,
+                    ail.product_id, ai.partner_id, ai.payment_term_id, ail.account_analytic_id,
+                    u2.name AS uom_name,
+                    ai.currency_id, ai.journal_id, ai.fiscal_position_id, ai.user_id, ai.company_id,
+                    1 AS nbr,
+                    ai.type, ai.state, pt.categ_id, ai.date_due, ai.account_id, ail.account_id AS account_line_id,
+                    ai.partner_bank_id,
+                    SUM ((invoice_type.sign * ail.quantity) / u.factor * u2.factor) AS product_qty,
+                    SUM(ail.price_subtotal * invoice_type.sign) AS price_total,
+                    SUM(ail.price_subtotal_net * invoice_type.sign) AS price_total_net,
+                    SUM(ABS(ail.price_subtotal_signed)) / CASE
+                            WHEN SUM(ail.quantity / u.factor * u2.factor) <> 0::numeric
+                               THEN SUM(ail.quantity / u.factor * u2.factor)
+                               ELSE 1::numeric
+                            END AS price_average,
+                    ai.residual_company_signed / (SELECT count(*) FROM account_invoice_line l where invoice_id = ai.id) *
+                    count(*) * invoice_type.sign AS residual,
+                    ai.commercial_partner_id as commercial_partner_id,
+                    partner.country_id
+        """
+        return select_str
