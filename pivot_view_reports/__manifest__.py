0286a9d95b5bdcbf8ed7e08ef7f8522491f9fde4
Claire CAPARROS <ccaparros@yziact.fr>
2018-11-20 11:13:27 +0100

    [CHG] Divers modif vues
    Regroupement des vues pivot en un module et ajout de l'équipe de vente sur les devis/factures

---
+++ pivot_view_reports/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+{
+    'name': "Vues pivot",
+
+    'summary': """ Modification des vues pivot """,
+
+    'description': """ Modification des vues pivot """,
+
+    'author': "Yziact",
+    'maintainer': "C. CAPARROS",
+
+    'website': "http://yziact.fr/",
+
+    'version': '0.1',
+
+    'category': 'Other',
+
+    'depends': [
+        'account',
+        'base',
+    ],
+
+    'data': [
+
+    ],
+
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
