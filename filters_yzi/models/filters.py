ae4a625fc70b16989a14245fd45ef4e988e4af8f
Baptiste <bchaudat@yziact.fr>
2018-12-04 19:32:02 +0100

    [FIX]missing code during release

--- filters_yzi/models/filters.py
+++ filters_yzi/models/filters.py
@@ -9,16 +9,19 @@ from odoo import models, api, fields, _, tools
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    partner_id = fields.Many2one('res.partner', delegate=True)
+    gr = fields.Many2one('dom.groupe', string='Groupe', related='partner_id.gr')
+    sgr = fields.Many2one('dom.sous.groupe', string='Sous Groupe', related='partner_id.sgr')
 
 
-class SaleReport(models.Model):
-    _inherit = 'sale.report'
 
-    partner_id = fields.Many2one('res.partner', delegate=True)
+# class SaleReport(models.Model):
+#     _inherit = 'sale.report'
+#
+#     partner_id = fields.Many2one('res.partner', delegate=True)
 
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    partner_id = fields.Many2one('res.partner', delegate=True)
+    gr = fields.Many2one('dom.groupe', string='Groupe', related='partner_id.gr')
+    sgr = fields.Many2one('dom.sous.groupe', string='Sous Groupe', related='partner_id.sgr')
