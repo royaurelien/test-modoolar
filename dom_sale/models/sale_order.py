d850613adf05ad14b65cee66d29a07bab2db8ce7
Claire CAPARROS <ccaparros@yziact.fr>
2019-10-30 18:06:55 +0100

    [CHG] add import

--- dom_sale/models/sale_order.py
+++ dom_sale/models/sale_order.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, tools
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 from odoo.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT
 
 
